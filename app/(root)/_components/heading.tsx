c549f7d92c7fb12b3673c38ef0678f04faec3833
Shibraj <shibrajdeb456@gmail.com>
2024-01-07 17:24:27 +0530

    added auth using clerk and convex

--- app/(root)/_components/heading.tsx
+++ app/(root)/_components/heading.tsx
@@ -1,24 +1,44 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react"
+import Link from "next/link";
 
 
 export default function Heading() {
+  const { isLoading, isAuthenticated } = useConvexAuth()
   return (
     <div className="max-w-3xl space-y-4">
-       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-         Your Ideas, Documents & Plans, Unified. Welcome to <span className="underline">Motion</span>
-       </h1>
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents & Plans, Unified. Welcome to <span className="underline">Motion</span>
+      </h1>
 
-       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-          Motion is the connected workspace where <br />
-          better, faster work happens
-       </h3>
-       <Button>
-        Enter Motion
-        <ArrowRight className="h-4 w-4 ml-2"/>
-       </Button>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Motion is the connected workspace where <br />
+        better, faster work happens
+      </h3>
+      {isLoading && (
+        <Spinner />
+      )}
+      {!isAuthenticated && !isLoading && (
+        <Button>
+          Get Notion Free
+          <ArrowRight className="h-4 w-4 ml-2" />
+        </Button>
+      )}
+
+      {isAuthenticated && !isLoading && (
+        <Button>
+          <Link href="/documents">
+            Enter Motion
+          </Link>
+          <ArrowRight className="h-4 w-4 ml-2" />
+        </Button>
+      )}
+
+
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from "@/components/providers/theme-provider"
+import { ConvexClientProvider } from '@/components/providers/convex-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,6 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
+        <ConvexClientProvider>
       <ThemeProvider
             attribute="class"
             defaultTheme="system"
@@ -39,6 +41,7 @@ export default function RootLayout({
           >
         {children}
         </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   )
