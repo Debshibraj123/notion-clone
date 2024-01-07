d598489782b53810dde31b25dd0248a1e3902ad0
Shibraj <shibrajdeb456@gmail.com>
2024-01-07 23:49:46 +0530

    added user setting using clerk

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react"
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 
 export default function DocumentsPage() {
+
+  const { user } = useUser();
   return (
-    <div>
-      
+    <div className='h-full flex flex-col items-center justify-center'>
+       <Image src="/empty.png" height="300" width="300" alt="Empty" className="dark:hidden" />
+       <Image src="/empty-dark.png" height="300" width="300" alt="Empty" className=" hidden dark:block" />
+
+       <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotion 
+       </h2>
+       <Button>
+         <PlusCircle className="h-4 w-4 mr-2" />
+         Create a note
+       </Button>
     </div>
   )
 }
